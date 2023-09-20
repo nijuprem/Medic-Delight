029c74115382043137bf020729e2b9665313e948
niju.prem <nijuprem7@gmail.com>
2023-09-20 21:37:02 +0530

    Initialized Client End

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Heading>Hello</Heading>
+    </div>
+  );
+}
+
+export default App;
