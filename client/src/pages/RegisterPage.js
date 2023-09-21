cfc6adc6930714d0844c46b6fcd0eb0452b6989e
niju.prem <nijuprem7@gmail.com>
2023-09-21 14:22:59 +0530

    User Registeration Page Added

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,10 +1,86 @@
 import React from "react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Box,
+  Flex,
+  Heading,
+  Center,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { useFormik } from "formik";
 
 const Register = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      secondPassword: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Flex justifyContent={"center"} height={"auto"} mt={"5%"}>
+      <Box w={"34%"} p={5} border={"1px solid black"} borderRadius={"1rem"}>
+        <Heading as="h3" textAlign={"center"}>
+          Registration Page
+        </Heading>
+        <form onSubmit={formik.handleSubmit}>
+          <FormControl textAlign={"center"}>
+            <FormLabel mt={4}>Name</FormLabel>
+            <Input
+              type="name"
+              name="name"
+              placeholder="Your Name"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+            />
+            <FormLabel mt={4}>Email</FormLabel>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+            <FormLabel mt={4}>Password</FormLabel>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+            <FormLabel mt={4}>Re-enter Password</FormLabel>
+            <Input
+              type="password"
+              name="secondPassword"
+              placeholder="Password"
+              onChange={formik.handleChange}
+              value={formik.values.secondPassword}
+            />
+            <Center>
+              <Button
+                mt={3}
+                display={"block"}
+                type="submit"
+                background="teal.300"
+              >
+                Submit
+              </Button>
+            </Center>
+            <Box mt={3}>
+              <Link to="/login">Already a user? Login here</Link>
+            </Box>
+          </FormControl>
+        </form>
+      </Box>
+    </Flex>
   );
 };
 
