ba78f2583ab67395af1d394736e9d060155b02a5
niju.prem <nijuprem7@gmail.com>
2023-09-21 15:53:22 +0530

    Added user Controller. Route and Model

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  secondpassword: {
+    type: String,
+    required: [true, "password is required"],
+  },
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
--- server.js
+++ server.js
@@ -15,11 +15,7 @@ app.use(morgan("dev"));
 
 // routes
 
-app.get("/", (req, res) => {
-  res.status(200).send({
-    message: "server running",
-  });
-});
+app.use("/api/v1/user", require("./routes/userRoute"));
 
 const port = process.env.PORT;
 
